a6572dee99a5879b8d17b9e8062c69acca306be7
Pallavinatraj <pallavinatraj31@gmail.com>
2024-11-20 21:39:20 +0530

    Assignments

---
+++ 20-11-24/file.js
@@ -0,0 +1,276 @@
+var mensData = [
+    {
+        image_url:
+            "https://images.puma.com/image/upload/f_auto,q_auto,b_rgb:fafafa,w_600,h_600/global/107687/03/sv01/fnd/IND/fmt/png/ULTRA-5-MATCH-FG/AG-Unisex-Football-Boots",
+        name: "MEN'S REEBOK RUNNING ROADMAP SHOES",
+        price: "1170",
+        strikedoffprice: "2599",
+    },
+    {
+        image_url:
+            "https://images.puma.com/image/upload/f_auto,q_auto,b_rgb:fafafa,w_600,h_600/global/107687/03/sv01/fnd/IND/fmt/png/ULTRA-5-MATCH-FG/AG-Unisex-Football-Bootstatic/Product-EX3921/reebok_EX3921_1.jpg.plp",
+        name: "MEN'S REEBOK SWIM ARUBA FLIP SLIPPERS ",
+        price: 699,
+        strikedoffprice: 999,
+    },
+    {
+        image_url:
+            "https://images.puma.com/image/upload/f_auto,q_auto,b_rgb:fafafa,w_600,h_600/global/107687/03/sv01/fnd/IND/fmt/png/ULTRA-5-MATCH-FG/AG-Unisex-Football-Boots",
+        name: " MEN'S REEBOK RUNNING AUSTIN SHOES",
+        price: 945,
+        strikedoffprice: 2099,
+    },
+    {
+        image_url:
+            "https://images.puma.com/image/upload/f_auto,q_auto,b_rgb:fafafa,w_600,h_600/global/107687/03/sv01/fnd/IND/fmt/png/ULTRA-5-MATCH-FG/AG-Unisex-Football-Boots",
+        name: "MEN'S REEBOK SWIM AVENGER FLIP LP SLIPPERS",
+        price: 399,
+        strikedoffprice: 799,
+    },
+    {
+        image_url:
+            "https://images.puma.com/image/upload/f_auto,q_auto,b_rgb:fafafa,w_600,h_600/global/107687/03/sv01/fnd/IND/fmt/png/ULTRA-5-MATCH-FG/AG-Unisex-Football-Bootshttps://content.shop4reebok.com/static/Product-EW5173/reebok_EW5173_1.jpg.plp",
+        name: "MEN'S REEBOK RUNNING TEMPO SHOES ",
+        price: 1620,
+        strikedoffprice: 3599,
+    },
+    {
+        image_url:
+            "https://images.puma.com/image/upload/f_auto,q_auto,b_rgb:fafafa,w_600,h_600/global/107687/03/sv01/fnd/IND/fmt/png/ULTRA-5-MATCH-FG/AG-Unisex-Football-Boots ",
+        name: " MEN'S REEBOK RUNNING ASTRO BOOSTER SHOES ",
+        price: 1440,
+        strikedoffprice: 3199,
+    },
+    {
+        image_url:
+            "https://images.puma.com/image/upload/f_auto,q_auto,b_rgb:fafafa,w_600,h_600/global/107687/03/sv01/fnd/IND/fmt/png/ULTRA-5-MATCH-FG/AG-Unisex-Football-Boots",
+        name: " MEN'S TRAINING WORKOUT READY ELITAGE PANTS ",
+        price: 1999,
+        strikedoffprice: 3199,
+    },
+    {
+        image_url:
+            "https://images.puma.com/image/upload/f_auto,q_auto,b_rgb:fafafa,w_600,h_600/global/107687/03/sv01/fnd/IND/fmt/png/ULTRA-5-MATCH-FG/AG-Unisex-Football-Boots ",
+        name: "MEN'S REEBOK TRAINING  POLO T-SHIRTS TEE  ",
+        price: 769,
+        strikedoffprice: 1099,
+    },
+    {
+        image_url:
+            "https://images.puma.com/image/upload/f_auto,q_auto,b_rgb:fafafa,w_600,h_600/global/107687/03/sv01/fnd/IND/fmt/png/ULTRA-5-MATCH-FG/AG-Unisex-Football-Boots",
+        name: "MEN'S REEBOK TRAINING WORKOUT  TEE  ",
+        price: 1299,
+        strikedoffprice: 1440,
+    },
+    {
+        image_url:
+            "https://images.puma.com/image/upload/f_auto,q_auto,b_rgb:fafafa,w_600,h_600/global/107687/03/sv01/fnd/IND/fmt/png/ULTRA-5-MATCH-FG/AG-Unisex-Football-Boots",
+        name: "MEN'S REEBOK SUPER SOFT FLIP SLIPPERS",
+        price: 699,
+        strikedoffprice: 999,
+    },
+    {
+        image_url:
+            "https://images.puma.com/image/upload/f_auto,q_auto,b_rgb:fafafa,w_600,h_600/global/107687/03/sv01/fnd/IND/fmt/png/ULTRA-5-MATCH-FG/AG-Unisex-Football-Boots",
+        name: "MEN'S REEBOK RUNNING RUN SHOES  ",
+        price: 1620,
+        strikedoffprice: 3599,
+    },
+    {
+        image_url:
+            "https://content.shop4reebok.com/static/Product-EX4285/reebok_EX4285_1.jpg.plp ",
+        name: "MEN'S REEBOK RUNNING GENESIS RUNNER SHOES  ",
+        price: 1620,
+        strikedoffprice: 3599,
+    },
+    {
+        image_url:
+            "https://content.shop4reebok.com/static/Product-FV9519/MEN_SWIM_SLIPPERS_FV9519_1.jpg.plp ",
+        name: "MEN'S REEBOK SWIM RUSSLE FLIP SLIPPERS  ",
+        price: 599,
+        strikedoffprice: 999,
+    },
+    {
+        image_url:
+            "https://content.shop4reebok.com/static/Product-FW0318/reebok_FW0318_1.jpg.plp ",
+        name: " MEN'S REEBOK RUNNING TREND RUNNER SHOES",
+        price: 1575,
+        strikedoffprice: 3499,
+    },
+    {
+        image_url:
+            "https://content.shop4reebok.com/static/Product-H00024/reebok_H00024_1.jpg.plp ",
+        name: "UNISEX REEBOK RUNNING ZIG KINETICA II SHOES  ",
+        price: 9999,
+        strikedoffprice: 12000,
+    },
+    {
+        image_url:
+            "https://content.shop4reebok.com/static/Product-GK9170/reebok_GK9170_1.jpg.plp ",
+        name: " MEN'S REEBOK TRAINING BAS POLO TEE ",
+        price: 779,
+        strikedoffprice: 1299,
+    },
+    {
+        image_url:
+            "https://content.shop4reebok.com/static/Product-GK9175/reebok_GK9175_1.jpg.plp",
+        name: "MEN'S REEBOK TRAINING FOUNDATION  PANTS  ",
+        price: 1019,
+        strikedoffprice: 1699,
+    },
+    {
+        image_url:
+            "https://content.shop4reebok.com/static/Product-GK9176/reebok_GK9176_1.jpg.plp ",
+        name: "MEN'S REEBOK TRAINING FOUNDATION PANTS  ",
+        price: 1019,
+        strikedoffprice: 1699,
+    },
+    {
+        image_url:
+            "https://content.shop4reebok.com/static/Product-FW0318/reebok_FW0318_1.jpg.plp ",
+        name: " MEN'S REEBOK RUNNING TREND SHOES",
+        price: 1575,
+        strikedoffprice: 3499,
+    },
+    {
+        image_url:
+            " https://content.shop4reebok.com/static/Product-G57776/reebok_G57776_1.jpg.plp ",
+        name: "  MEN'S REEBOK RUNNING ZTAUR RUN SHOES ",
+        price: 3499,
+        strikedoffprice: 4999,
+    },
+    {
+        image_url:
+            " https://content.shop4reebok.com/static/Product-FV9555/MEN_SWIM_SLIPPERS_FV9555_1.jpg.plp  ",
+        name: "MEN'S REEBOK SWIM ULTRA FLIP SLIPPERS   ",
+        price: 749,
+        strikedoffprice: 1499,
+    },
+    {
+        image_url:
+            "https://content.shop4reebok.com/static/Product-EX3994/reebok_EX3994_1.jpg.plp   ",
+        name: "  MEN'S REEBOK SWIM FLEX CONNECT SANDALS ",
+        price: 1149,
+        strikedoffprice: 2299,
+    },
+    {
+        image_url:
+            " https://content.shop4reebok.com/static/Product-EX4287/reebok_EX4287_1.jpg.plp  ",
+        name: " MEN'S REEBOK RUNNING GENESIS RUNNER SHOE  ",
+        price: 1620,
+        strikedoffprice: 3599,
+    },
+    {
+        image_url:
+            "https://images.puma.com/image/upload/f_auto,q_auto,b_rgb:fafafa,w_600,h_600/global/107687/03/sv01/fnd/IND/fmt/png/ULTRA-5-MATCH-FG/AG-Unisex-Football-Boots",
+        name: "MEN'S REEBOK RUNNING RUN FUSION  SHOES",
+        price: 1620,
+        strikedoffprice: 3599,
+    },
+    {
+        image_url:
+            "https://images.puma.com/image/upload/f_auto,q_auto,b_rgb:fafafa,w_600,h_600/global/107687/03/sv01/fnd/IND/fmt/png/ULTRA-5-MATCH-FG/AG-Unisex-Football-Boots",
+        name: "MEN'S REEBOK RUNNING RUN FUSION  SHOES",
+        price: 1620,
+        strikedoffprice: 3599,
+    },
+    {
+        image_url:
+            "https://images.puma.com/image/upload/f_auto,q_auto,b_rgb:fafafa,w_600,h_600/global/107687/03/sv01/fnd/IND/fmt/png/ULTRA-5-MATCH-FG/AG-Unisex-Football-Boots",
+        name: "MEN'S REEBOK RUNNING RUN FUSION  SHOES",
+        price: 1620,
+        strikedoffprice: 3599,
+    },
+    {
+        image_url:
+            "https://images.puma.com/image/upload/f_auto,q_auto,b_rgb:fafafa,w_600,h_600/global/107687/03/sv01/fnd/IND/fmt/png/ULTRA-5-MATCH-FG/AG-Unisex-Football-Boots",
+        name: "MEN'S REEBOK RUNNING RUN FUSION  SHOES",
+        price: 1620,
+        strikedoffprice: 3599,
+    },
+    {
+        image_url:
+            "https://images.puma.com/image/upload/f_auto,q_auto,b_rgb:fafafa,w_600,h_600/global/107687/03/sv01/fnd/IND/fmt/png/ULTRA-5-MATCH-FG/AG-Unisex-Football-Boots",
+        name: "MEN'S REEBOK RUNNING RUN FUSION  SHOES",
+        price: 1620,
+        strikedoffprice: 3599,
+    },
+    {
+        image_url:
+            "https://images.puma.com/image/upload/f_auto,q_auto,b_rgb:fafafa,w_600,h_600/global/107687/03/sv01/fnd/IND/fmt/png/ULTRA-5-MATCH-FG/AG-Unisex-Football-Boots",
+        name: "MEN'S REEBOK RUNNING RUN FUSION  SHOES",
+        price: 1620,
+        strikedoffprice: 3599,
+    },
+    {
+        image_url:
+            "https://images.puma.com/image/upload/f_auto,q_auto,b_rgb:fafafa,w_600,h_600/global/107687/03/sv01/fnd/IND/fmt/png/ULTRA-5-MATCH-FG/AG-Unisex-Football-Boots",
+        name: "MEN'S REEBOK RUNNING RUN FUSION  SHOES",
+        price: 1620,
+        strikedoffprice: 3599,
+    },
+    {
+        image_url:
+            "https://content.shop4reebok.com/static/Product-EX4272/reebok_EX4272_1.jpg.plp",
+        name: "MEN'S REEBOK RUNNING QUICK DRIFTER SHOES",
+        price: 1620,
+        strikedoffprice: 3599,
+    },
+    {
+        image_url:
+            "https://images.puma.com/image/upload/f_auto,q_auto,b_rgb:fafafa,w_600,h_600/global/107687/03/sv01/fnd/IND/fmt/png/ULTRA-5-MATCH-FG/AG-Unisex-Football-Boots",
+        name: "MEN'S REEBOK DYNAMIC ZIGZAG SHOES",
+        price: 7599,
+        strikedoffprice: 9999,
+    },
+    {
+        image_url:
+            "https://images.puma.com/image/upload/f_auto,q_auto,b_rgb:fafafa,w_600,h_600/global/107687/03/sv01/fnd/IND/fmt/png/ULTRA-5-MATCH-FG/AG-Unisex-Football-Boots",
+        name: "MEN'S REEBOK SWIM SUPERSOFT FLIP SLIPPERS",
+        price: 909,
+        strikedoffprice: 1299,
+    },
+    {
+        image_url:
+            "https://images.puma.com/image/upload/f_auto,q_auto,b_rgb:fafafa,w_600,h_600/global/107687/03/sv01/fnd/IND/fmt/png/ULTRA-5-MATCH-FG/AG-Unisex-Football-Boots",
+        name: "MEN'S REEBOK TRAINING ACTIVE BASIC POLO TEE",
+        price: 779,
+        strikedoffprice: 1299,
+    },
+    {
+        image_url:
+            "https://images.puma.com/image/upload/f_auto,q_auto,b_rgb:fafafa,w_600,h_600/global/107687/03/sv01/fnd/IND/fmt/png/ULTRA-5-MATCH-FG/AG-Unisex-Football-Boots",
+        name: "MEN'S REEBOK TRAINING ACTIVE MOVE TEE ",
+        price: 1749,
+        strikedoffprice: 2499,
+    },
+    {
+        image_url:
+            "https://images.puma.com/image/upload/f_auto,q_auto,b_rgb:fafafa,w_600,h_600/global/107687/03/sv01/fnd/IND/fmt/png/ULTRA-5-MATCH-FG/AG-Unisex-Football-Boots",
+        name: "MEN'S REEBOK FITNESS TRAINING  SWEATSHIRT",
+        price: 1079,
+        strikedoffprice: 1799,
+    },
+];
+
+mensData.map(function (mensData) {
+    let box = document.createElement("div");
+    box.innerHTML = `
+        <img src="${elem.image_url}" alt="${elem.name}">
+        <p>${elem.name}</p>
+        <p><span class="price">₹${elem.price}</span> <span class="striked-price">₹${elem.strikedoffprice}</span></p>
+        <button>Add to Cart</button>
+    `;
+    document.querySelector("body").append(box);
+});
+
+document.querySelector("button").addEventListener("click", addToCart);
+
+
+function addToCart(elem) {
+    localStorage.setItem("item", JSON.stringify(elem));
+    console.log(elem);
+
+
+
+}
